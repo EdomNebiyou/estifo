e2f028a783e831fb73ff1d44ef69ea4040fca2bb
edom nebiyou <edomnebiyou858@gmail.com>
2025-02-05 21:10:34 +0300

    dia

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { Projects } from "./components/Projects/Projects";
 function App() {
   return (
     <div className={styles.App}>
+      <h1>i</h1>
       <Navbar />
       <Hero />
       <About />
